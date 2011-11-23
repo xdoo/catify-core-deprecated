444354d3ae955c684595119e8c39ca64a9a22407
Claus Straube <info@catify.com>
2011-11-23 08:34:54 +0100

    created test case for #28

--- catify-core/src/test/java/com/catify/core/process/TestReceiveNode.java
+++ catify-core/src/test/java/com/catify/core/process/TestReceiveNode.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.catify.core.process;
 
 import java.util.concurrent.TimeUnit;
@@ -9,6 +25,7 @@ import org.junit.Test;
 
 import com.catify.core.constants.CacheConstants;
 import com.catify.core.process.model.ProcessDefinition;
+import com.catify.core.process.processors.ReadCorrelationProcessor;
 import com.catify.core.testsupport.SpringTestBase;
 import com.hazelcast.core.Hazelcast;
 
@@ -45,7 +62,7 @@ public class TestReceiveNode extends SpringTestBase {
 	@Test
 	public void testReceiveWithWait() throws Exception{		
 		this.deploy();
-		this.createMessageCopyRoute(1500);
+		this.createAdditionalRoutes(1500);
 		
 		out.setExpectedMessageCount(1);
 		timeout.setExpectedMessageCount(0);
@@ -64,7 +81,7 @@ public class TestReceiveNode extends SpringTestBase {
 	@Test
 	public void testReceiveWithoutWait() throws Exception{		
 		this.deploy();
-		this.createMessageCopyRoute(500);
+		this.createAdditionalRoutes(500);
 		
 		out.setExpectedMessageCount(1);
 		timeout.setExpectedMessageCount(0);		
@@ -85,7 +102,7 @@ public class TestReceiveNode extends SpringTestBase {
 	@Test
 	public void testReceiveTimeOut() throws Exception{		
 		this.deploy();
-		this.createMessageCopyRoute(6000);
+		this.createAdditionalRoutes(6000);
 		
 		out.setExpectedMessageCount(0);
 		timeout.setExpectedMessageCount(1);
@@ -93,7 +110,7 @@ public class TestReceiveNode extends SpringTestBase {
 		//send message 
 		template.sendBody("seda:init_process", super.getXml());
 		
-		assertMockEndpointsSatisfied(10, TimeUnit.SECONDS);
+		assertMockEndpointsSatisfied(100, TimeUnit.SECONDS);
 		
 	}
 	
@@ -116,7 +133,38 @@ public class TestReceiveNode extends SpringTestBase {
 		assertMockEndpointsSatisfied(10, TimeUnit.SECONDS);
 	}
 	
-	private void createMessageCopyRoute(final int delay) throws Exception{
+	@Test public void testCorrelationException() throws InterruptedException {
+		super.deployProcess(this.processCorrelationException());
+		
+		out.setExpectedMessageCount(1);
+		out.expectedBodiesReceived(this.getNumberXml());
+		
+		template.sendBody("seda://init", this.getXml());		
+		Thread.sleep(500);
+		template.sendBody("seda://in", this.getNumberXml());
+		
+		assertMockEndpointsSatisfied(10, TimeUnit.SECONDS);
+	}
+	
+	@Test public void testCorrelationExceptionWithReCall() throws Exception {
+		super.deployProcess(this.processReCallProcessAfterCorrelationException());
+		this.createAdditionalRoutes(1000);
+		
+		out.setExpectedMessageCount(1);
+		out.expectedBodiesReceived(this.getNumberXml());
+		
+		template.sendBody("seda://init", this.getXml());
+		Thread.sleep(1000);
+		template.sendBody("seda:in", this.getNumberXml());
+		
+		// give the process time to throw an exception again (it should not ;)
+		Thread.sleep(10000);
+		
+		assertMockEndpointsSatisfied(5, TimeUnit.SECONDS);
+		
+	}
+	
+	private void createAdditionalRoutes(final int delay) throws Exception{
 
 		RouteBuilder builder = new RouteBuilder(){
 
@@ -126,6 +174,16 @@ public class TestReceiveNode extends SpringTestBase {
 				.delay(delay)
 				.log("-------------------------------------------> returned message")
 				.to("seda:in");	
+				
+				from("seda:corr_ex")
+				.setHeader(ReadCorrelationProcessor.CORRELATION_EXCEPTION_HEADER, constant(""))
+				.to("log:CORR_EX?showAll=true")
+				.to("seda:init")
+				.to("log:RECALL?showAll=true")
+				.delay(delay)
+				.to("seda:in")
+				.to("log:SEND_MESSAGE_AGAIN?showAll=true");
+				
 			}			
 		};
 		
@@ -219,6 +277,71 @@ public class TestReceiveNode extends SpringTestBase {
 				"	</request>\n" +
 				"	<end ns:name=\"end\"/>\n" +
 				"</process>";
+	}
+	
+	private String processCorrelationException(){
+		return 	" <process processVersion=\"1.0\" processName=\"process02\" accountName=\"CATIFY\" xmlns=\"http://www.catify.com/api/1.0\" xmlns:ns=\"http://www.catify.com/api/1.0\" >\n" +
+		"	<start ns:name=\"start\">\n" +
+		"		<inPipeline>\n" +
+		"			<endpoint ns:uri=\"seda:init\"/>\n" +
+		"			<correlation>\n" +
+		"				<xpath>/foo/a</xpath>\n" +
+		"				<xpath>/foo/b</xpath>\n" +
+		"			</correlation>\n" +
+		"		</inPipeline>\n" +
+		"	</start>\n" +
+		"	<receive ns:name=\"wait_for_payload\">\n" +
+		"		<inPipeline>\n" +
+		"			<pipelineExceptionEvent ns:uri=\"mock:out\" ns:attachPayload=\"true\"/>\n" +
+		"			<endpoint ns:uri=\"seda:in\"/>\n" +
+		"			<variables>\n" +
+		"				<variable ns:name=\"foo\" ns:xpath=\"/\" />\n" +
+		"			</variables>\n" +
+		"			<correlation>\n" +
+		"				<xpath>/foo/a</xpath>\n" +
+		"				<xpath>/foo/b</xpath>\n" +
+		"			</correlation>\n" +
+		"		</inPipeline>\n" +
+		"	</receive>\n" +
+		"	<end ns:name=\"end\"/>\n" +
+		"</process>";
+		
+	}
+	
+	private String processReCallProcessAfterCorrelationException(){
+		return 	" <process processVersion=\"1.0\" processName=\"process01\" accountName=\"CATIFY\" xmlns=\"http://www.catify.com/api/1.0\" xmlns:ns=\"http://www.catify.com/api/1.0\" >\n" +
+		"	<start ns:name=\"start\">\n" +
+		"		<inPipeline>\n" +
+		"			<endpoint ns:uri=\"seda:init\"/>\n" +
+		"			<correlation>\n" +
+		"				<xpath>/foo/a</xpath>\n" +
+		"				<xpath>/foo/b</xpath>\n" +
+		"			</correlation>\n" +
+		"		</inPipeline>\n" +
+		"	</start>\n" +
+		"	<receive ns:name=\"wait_for_payload\">\n" +
+		"		<inPipeline>\n" +
+		"			<pipelineExceptionEvent ns:uri=\"seda:corr_ex\" ns:attachPayload=\"true\"/>\n" +
+		"			<endpoint ns:uri=\"seda:in\"/>\n" +
+		"			<variables>\n" +
+		"				<variable ns:name=\"foo\" ns:xpath=\"/\" />\n" +
+		"			</variables>\n" +
+		"			<correlation>\n" +
+		"				<xpath>/foo/a</xpath>\n" +
+		"				<xpath>/foo/b</xpath>\n" +
+		"			</correlation>\n" +
+		"		</inPipeline>\n" +
+		"	</receive>\n" +
+		"	<request ns:name=\"mock_p2\">\n" +
+		"		<outPipeline>\n" +
+		"			<endpoint ns:uri=\"mock://out\"/>\n" +
+		"			<variables>\n" +
+		"				<variable ns:name=\"foo\" />\n" +
+		"			</variables>\n" +
+		"		</outPipeline>\n" +
+		"	</request>\n" +
+		"	<end ns:name=\"end\"/>\n" +
+		"</process>";
 	}
 
 }
