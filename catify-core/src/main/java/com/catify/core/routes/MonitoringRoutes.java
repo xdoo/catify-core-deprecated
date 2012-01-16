55fe3f0ae7e2780a6176fb2e76468f6e95139251
Claus Straube <info@catify.com>
2012-01-16 17:52:19 +0100

    added monitoring for hazelcast (not tested)

---
+++ catify-core/src/main/java/com/catify/core/routes/MonitoringRoutes.java
@@ -0,0 +1,16 @@
+package com.catify.core.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class MonitoringRoutes extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		
+		from("timer://monitor?fixedRate=true&period=60000")
+		.routeId("monitor-event")
+		.beanRef("hazelcastMonitor");
+		
+	}
+
+}
